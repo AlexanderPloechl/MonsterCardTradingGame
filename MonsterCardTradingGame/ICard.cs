87bf87dc6c0c89850a4f9f8506386eec5082bcd5
AlexanderPloechl <if19b086@technikum-wien.at>
2021-10-12 10:55:49 +0200

    Add project files.

---
+++ MonsterCardTradingGame/ICard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterCardTradingGame
+{
+    interface ICard
+    {
+        public string name { get; set; }
+        public int damage { get; }
+        public ElementType elementType { get; set; }
+
+        void attack();
+    }
+}

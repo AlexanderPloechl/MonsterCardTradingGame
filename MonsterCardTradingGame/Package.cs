6ecf2ebb54acfb773cffe435921470dccc3741e7
AlexanderPloechl <if19b086@technikum-wien.at>
2021-10-19 20:56:29 +0200

    first kind of working package protoyp

--- MonsterCardTradingGame/Package.cs
+++ MonsterCardTradingGame/Package.cs
@@ -6,35 +6,22 @@ using System.Threading.Tasks;
 
 namespace MonsterCardTradingGame
 {
-    class Package //: IMonster, ISpell
+    class Package
     {
-        //public string name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        //public int damage => throw new NotImplementedException();
-
-        //public ElementType elementType { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        //public ElementType ElementType { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        //public MonsterType MonsterType { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        //public void attack()
-        //{
-        //    throw new NotImplementedException();
-        //}
+        const int NumberOfCardsInPackage = 5;
         List<ICard> CardsInPackage;
         public Package()
         {
             CardsInPackage = new List<ICard>();
-            Monster monster = new Monster();
-            CardsInPackage.Add(monster);
+            for (int i = 0; i < NumberOfCardsInPackage; i++)
+            {
+                CardsInPackage.Add(new Monster());
+            }
         }
-        void AddCardToList()
-        {
-            Monster test = new Monster();
-            CardsInPackage.Add(test);
-        }
+
         public void PrintPackageContents()
         {
-            foreach(ICard Card in CardsInPackage)
+            foreach (ICard Card in CardsInPackage)
             {
                 Console.WriteLine(Card.name);
             }

a4e9b534a93bea70ba06f6ea6b7127d7743f1317
AlexanderPloechl <if19b086@technikum-wien.at>
2021-10-22 12:15:27 +0200

    package prototyp finished + added Spell.cs

--- MonsterCardTradingGame/Package.cs
+++ MonsterCardTradingGame/Package.cs
@@ -13,17 +13,22 @@ namespace MonsterCardTradingGame
         public Package()
         {
             CardsInPackage = new List<ICard>();
-            for (int i = 0; i < NumberOfCardsInPackage; i++)
-            {
-                CardsInPackage.Add(new Monster());
-            }
+            //for (int i = 0; i < NumberOfCardsInPackage; i++)
+            //{
+            //    CardsInPackage.Add(new Monster());
+            //}
+            CardsInPackage.Add(new Monster("testmonster1", 2, MonsterType.Dragon));
+            CardsInPackage.Add(new Monster("testmonster2", 4, MonsterType.Wizard));
+            CardsInPackage.Add(new Spell("testspell1", 6, ElementType.Fire));
+            CardsInPackage.Add(new Spell("testspell2", 8, ElementType.Fire));
+            CardsInPackage.Add(new Spell("testspell3", 10, ElementType.Fire));
         }
 
         public void PrintPackageContents()
         {
             foreach (ICard Card in CardsInPackage)
             {
-                Console.WriteLine(Card.name);
+                Console.WriteLine($"{Card.name} - {Card.damage}");
             }
         }
     }
